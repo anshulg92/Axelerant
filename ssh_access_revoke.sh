230b3af27edc232b4f7a4ebdf12c8c2095d076ee
anshulg92 <46181019+anshulg92@users.noreply.github.com>
2019-05-02 15:41:03 +0530

    Update ssh_access_revoke.sh

--- ssh_access_revoke.sh
+++ ssh_access_revoke.sh
@@ -6,13 +6,13 @@ read -p "Please share ssh keys" keys
 
 serverlist=`cat ./serverlist`
 
-if [[ $access == access_revoke ]]
+if [[ $access == new_user ]]
 then
   for $server in $serverlist
   do
      ssh $server "useradd -m "$username" -s /bin/bash && echo "$keys" >> /home/$username/.ssh/authorized_keys"
   done
-elif [[ $access == new_user ]]
+elif [[ $access == access_revoke ]]
 then
   for $server in $serverlist
   do
