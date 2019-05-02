9ece85ab4302e57ddc66bcf7a2ae1b3620ccaf62
Anshg92 <anshul.gupta1992@yahoo.com>
2019-05-02 15:39:08 +0530

    Script for ssh access or revoke

---
+++ ssh_access_revoke.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Please provide username" username
+read -p "Please enter new_user or access_revoke" access
+read -p "Please share ssh keys" keys
+
+serverlist=`cat ./serverlist`
+
+if [[ $access == access_revoke ]]
+then
+  for $server in $serverlist
+  do
+     ssh $server "useradd -m "$username" -s /bin/bash && echo "$keys" >> /home/$username/.ssh/authorized_keys"
+  done
+elif [[ $access == new_user ]]
+then
+  for $server in $serverlist
+  do
+     ssh $server "userdel -r "$username""
+  done
+else
+   echo "Please enter new_user or access_revoke only"
+fi
